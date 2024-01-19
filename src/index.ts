634a97d57a0a2fbf172fa733cd22416ae3292ab0
Dmitriy <sova123409@gmail.com>
2024-01-19 16:56:04 +0300

    Init release-please

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { logger } from 'release-please/build/src/util/logger'
 import { builder } from './builder';
 
 export function init() {
-  registerPlugin('@ipfs-shipyard/release-please-ipfs-plugin', builder);
+  registerPlugin('release-please-scope-package-plugin', builder);
   logger.info('registered @educorg/release-please-scope-package-plugin');
 };
 
